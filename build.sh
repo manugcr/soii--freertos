717e90197f90fbf8b8b07311fd3d263377be8e1b
manugcr <mgilcernich@gmail.com>
2024-06-27 18:03:11 -0300

    CL: - Added build script - Ignored build files - Deleted some comments

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "  -> Moving to project directory"
+cd ./Demo/CORTEX_LM3S811_GCC/
+
+echo "  -> Clean old files"
+make clean
+
+echo "  -> Building project"
+make
+
+echo "  -> Launch QEMU"
+qemu-system-arm -machine lm3s811evb -kernel gcc/RTOSDemo.axf -serial stdio
